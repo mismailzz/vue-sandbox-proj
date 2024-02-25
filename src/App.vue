5ecd41b51198d55615bf346b1903090467de555a
Muhammad Ismail Zam Zam Butt <mismailzz75@gmail.com>
2024-02-25 20:07:57 +0100

    Learn simple example of v-bind and v-model

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,44 @@
 <script>
 import IncCounter from './components/IncCounter.vue'
+import BenSimpleExample from './components/BenSimpleExample.vue'
+
 export default {
   components: {
     IncCounter,
+    BenSimpleExample,
+  },
+  /*Adding @bencodezen code*/
+  data() {
+    return {
+      message: "Hello it works",
+      listOfNumbers: [
+        {
+          name: 1,
+          id: "6a887cd2-f0bf-4321-b192-92016f82a883",
+          list: [1, 2, 3],
+        },
+        {
+          name: 2,
+          id: "8d14d90b-2d47-473e-8293-d5c324111d0d",
+          list: [1, 2, 3],
+        },
+        {
+          name: 3,
+          id: "cd806d65-2309-4625-9104-dcd636cd79b5",
+          list: [1, 2, 3],
+        },
+        {
+          name: 4,
+          id: "00c939cd-fbf4-46d0-8e61-0bc2ce8a5332",
+          list: [1, 2, 3],
+        },
+        {
+          name: 5,
+          id: "295a8170-59c1-4462-9de1-3c9cd41cedab",
+          list: [1, 2, 3],
+        },
+      ],
+    };
   },
 }
 
@@ -11,5 +47,8 @@ export default {
 <template>
   <div id="app">
     <IncCounter />
+    <BenSimpleExample />
+
+    
   </div>
 </template>
---
+++ src/components/BenSimpleExample.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  data: () => ({
+    textColor: "black",
+    message: '', // This will hold the text input value
+    checked: false,
+  }),
+  methods: {
+    changeColor(color) {
+      this.textColor = color; // Update the text color based on the button clicked
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+
+    <!-- v-bind for dynamically update the attributes like we are changing the color on click
+    or similarly we can do for the links-->
+    <h2>v-bind Example</h2>
+    <h4 :style="{ color: textColor }">Dynamic Text Color</h4>
+    <button @click="changeColor('red')">Red</button>
+
+    <h2>v-model Example</h2>
+    <!--
+      v-model is a Vue.js directive that creates a two-way binding on an input, textarea, or select
+      element. It automatically picks the correct way to update the element based on the input type.
+      This makes it very useful for creating forms or input fields that update the Vue instance's 
+      data in real-time, as the user types or changes values.
+    -->
+    <input v-model="message" placeholder="Type something here...">
+    <p>The message is: {{ message }}</p>
+
+    <!-- Ex.2 Checkbox Binding-->
+    <input type="checkbox" v-model="checked">
+    <label>Check me</label>
+    <p>Checked: {{ checked }} </p>
+
+
+
+ </div>
+</template>
