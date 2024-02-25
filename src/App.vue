1f784a22561c496740827faa637c452295347717
Muhammad Ismail Zam Zam Butt <mismailzz75@gmail.com>
2024-02-25 16:08:21 +0100

    simple counter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <script>
+  export default {
+    data(){
+      return {
+        increment_counter: 0,
+      }
+    },
+
+    methods: {
+      incrementMe(){
+        this.increment_counter += 1
+      }
+    },
+  }
 </script>
 
 <template>
-  <div>Hello World</div>
+  <div id="app">
+
+
+    <!-- Simple Counter -->
+    <p>Increment: {{ increment_counter }}</p>
+    <button @click="incrementMe"> Increment </button>
+
+  </div>
 </template>
